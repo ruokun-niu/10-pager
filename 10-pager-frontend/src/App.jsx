76ff155d334bd65206f42d5c72a8ba8ee8050f0c
Ruokun Niu <ruokunniu@microsoft.com>
2023-09-12 09:33:44 -0700

    added prompt

--- 10-pager-frontend/src/App.jsx
+++ 10-pager-frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import lens from "./assets/lens.png";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [prompt, updatePrompt] = useState(undefined);
+
+  const sendPrompt = async (event) => {
+    if (event.key !== "Enter") {
+      return;
+    }
+    console.log('prompt', prompt)
+  }
+
   return (
     <div className="app">
       <div className="app-container">
@@ -10,6 +20,8 @@ function App() {
             type="text"
             className="spotlight__input"
             placeholder="Ask me anything..."
+            onChange={(e) => updatePrompt(e.target.value)}
+            onKeyDown={(e) => sendPrompt(e)}
             style={{
               backgroundImage: `url(${lens})`,
             }}
